b65b54ba67bb95818d3c77b2f9f6754e7c5f2a1c
Mehran Dashti <mehran19.md@gmail.com>
2026-06-22 22:46:01 +0330

    feat: Delete extra files

--- app/filters/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.filters.base import BaseFilter, FilterFn
-from app.filters.example_filter import ExampleFilter, ExampleFilterParams
-
-__all__ = ["BaseFilter", "FilterFn", "ExampleFilter", "ExampleFilterParams"]
